da81d56749282757722064dba3f5cdc1dd14806b
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-06-24 23:44:42 +0200

    Scatter plots

--- Bayesian_LinearRegression_GolfPlayers.R
+++ Bayesian_LinearRegression_GolfPlayers.R
@@ -31,3 +31,23 @@ ggplot(datM, aes(x=DrivingDistance, y=FinishedFair)) +
 attach(datF);
 datF.lm <- lm(FinishedFair~DrivingDistance);
 summary(datF.lm);
+
+
+# Call:
+#   lm(formula = FinishedFair ~ DrivingDistance)
+# 
+# Residuals:
+#   Min       1Q   Median       3Q      Max 
+# -23.6777  -2.6583   0.9829   3.6346  10.2339 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)     130.89331   10.92765  11.978  < 2e-16 ***
+#   DrivingDistance  -0.25649    0.04424  -5.797 3.66e-08 ***
+#   ---
+#   Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+# 
+# Residual standard error: 5.246 on 155 degrees of freedom
+# Multiple R-squared:  0.1782,	Adjusted R-squared:  0.1729 
+# F-statistic: 33.61 on 1 and 155 DF,  p-value: 3.662e-08
+
